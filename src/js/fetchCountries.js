3fff9ac457b8714e03071cf4adde6f37961292a8
Oleg Usatyi <olegysik1@ukr.net>
2021-07-23 12:08:38 +0300

    fixed catch error

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,11 @@
 export default function fetchCountries(name) {
     return fetch(`https://restcountries.eu/rest/v2/name/${name}?fields=name;capital;population;flag;languages`)
-        .then(response => { return response.json() })
-        .then(data => data);
+        .then(response => {
+            if (response.ok) {
+               return response.json() 
+            }
+            throw new Error(response.statusText)
+        })
 };
 
  
